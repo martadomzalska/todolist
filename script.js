05f576a3e478023a797224c53a3749348555a015
martadomzalska <105391131+martadomzalska@users.noreply.github.com>
2022-12-27 16:23:57 +0100

    added initial styles

---
+++ script.js
@@ -0,0 +1,37 @@
+const list = document.querySelector(".list");
+const form = document.querySelector("form");
+const input = form.querySelector("input");
+const button = form.querySelector("button");
+
+form.addEventListener("submit", (ev) => {
+  ev.preventDefault();
+  list.insertAdjacentHTML(
+    "beforeend",
+    `<li class="list__element">
+         <p> ${input.value} </p>
+        <div>
+        <button class="done" type="button"></button>  
+        <button class="remove" type="button"></button>  
+        </div>
+    </li>`
+  );
+  form.reset();
+});
+
+list.addEventListener("click", (event) => {
+  const targetEl = event.target;
+  const currTar = event.currentTarget;
+  const task = event.target.parentNode.previousElementSibling;
+    const listElement = event.target.parentNode.parentNode;
+
+  if (targetEl.nodeName == "BUTTON" && targetEl.classList.contains("done")) {
+    const checked = document.querySelector(".done");
+    console.log(checked);
+      targetEl.classList.toggle("checked");
+      task.classList.toggle("done-task");
+    //Notiflix.Notify.success("Goooooooood Job");
+  }
+  if (targetEl.nodeName == "BUTTON" && targetEl.classList.contains("remove")) {
+    listElement.remove();
+  }
+});
